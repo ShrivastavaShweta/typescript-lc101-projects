7f00192145833a93855e21202841ca586deb4a2f
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2022-11-30 23:54:17 -0600

    added changes to 2 files

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -1,21 +1,25 @@
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
 
+import { SpaceLocation } from './SpaceLocation';
 
 // Part 1: Declare (5) Variables With Type
-
-
+//let spacecraftName: string = 'Determination';
+//let speedMph: number = 17500;
+let kilometersToMars: number = 225000000;
+let kilometersToTheMoon: number = 384400;
+// let milesPerKilometer: number = 0.621;
 
 // Part 2: Print Days to Mars
-
+//let milesToMars: number = kilometersToMars * milesPerKilometer;
+//let hoursToMars: number = milesToMars / speedMph;
+//let daysToMars: number = hoursToMars / 24;
 
 
 // Code an output statement here (use a template literal):
 
 
-
 // Part 3: Create a Function ("getDaysToLocation")
-
 
 
 // Move your output statement from part 2 here. Update the template literal to call
@@ -23,19 +27,40 @@
 
 
 
-
 // Part 4: Create a Spacecraft Class
+class Spacecraft{
+    milesPerKilometer: number = 0.621;
+    name: string;
+    speedMph: number;
+    
+    constructor(name: string, speedMph: number){
+        this.name = name;
+        this.speedMph = speedMph;
+    }
+    
+    getDaysToLocation(kilometersAway: number): number{
+        let milesAway: number = kilometersAway * this.milesPerKilometer;
+        let hoursToLocation: number = milesAway / this.speedMph;
+        let daysToLocation: number = hoursToLocation / 24;
+        return daysToLocation;
+    }
 
+    printDaysToLocation(location: SpaceLocation) {
+        console.log(`${this.name} would take ${this.getDaysToLocation(location.kilometersAway)} days to get to ${location.name}.`);
+     }
+}
 
 
 
 // Create an instance of the class here:
-
+let spaceShuttle = new Spacecraft('Determination', 17500);
 
 
 // Move your output statements from part 3 here. Update the template literals use the
 // instance of the class.
+console.log(`The spacecraft ${spaceShuttle.name} will take ${spaceShuttle.getDaysToLocation(kilometersToMars)} days to reach Mars`);
 
+console.log(`The spacecraft ${spaceShuttle.name} will take ${spaceShuttle.getDaysToLocation(kilometersToTheMoon)} days to reach Moon`);
 
 
 // Part 5: Export and Import the SpaceLocation Class
@@ -45,3 +70,6 @@
 // Add the printDaysToLocation function to the Spacecraft class.
 
 // Paste in the code from step 6 here:
+
+spaceShuttle.printDaysToLocation(new SpaceLocation('Mars', kilometersToMars));
+spaceShuttle.printDaysToLocation(new SpaceLocation('the Moon', kilometersToTheMoon));
